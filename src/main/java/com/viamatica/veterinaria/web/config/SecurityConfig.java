2d251dca2cd99b430fe16b1100fbfff955eb247b
Boris Silva <boryssilva0202@gmail.com>
2023-07-19 17:54:26 -0500

    Implemetacion de Jwt

--- src/main/java/com/viamatica/veterinaria/web/config/SecurityConfig.java
+++ src/main/java/com/viamatica/veterinaria/web/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.viamatica.veterinaria.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +20,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(customizeRequests -> {
                     customizeRequests
+                            .requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers(HttpMethod.GET, "/perfil/**").hasAnyRole("ADMIN", "AUDITOR")
                             .requestMatchers(HttpMethod.POST, "/perfil/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.PUT, "/perfil/**").hasRole("ADMIN")
@@ -33,6 +36,10 @@ public class SecurityConfig {
                 })
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
