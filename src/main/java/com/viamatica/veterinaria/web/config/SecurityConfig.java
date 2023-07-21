c069aac23ef9074a3deff1f53423d459313ab462
Iván Alexander Rendón Quijije <ivanrendonq@gmail.com>
2023-07-21 08:19:44 -0500

    Añadí seguridad a las rutas del modulo de Hospitalizacion

--- src/main/java/com/viamatica/veterinaria/web/config/SecurityConfig.java
+++ src/main/java/com/viamatica/veterinaria/web/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -38,6 +40,8 @@ public class SecurityConfig {
                             .requestMatchers(HttpMethod.POST, "/usuario/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.PUT, "/usuario/**").hasRole("ADMIN")
                             .requestMatchers(HttpMethod.DELETE,"/usuario/**").hasRole("ADMIN")
+                            .requestMatchers("/HosHospitalizacionPaciente").hasRole("ADMIN")
+                            .requestMatchers("/HosCirugia").hasRole("ADMIN")
                             .anyRequest()
                             .authenticated();
 
